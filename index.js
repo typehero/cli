419e1a4dc3073a354359e360d4b0a04e77483560
Sean Boult <996134+Hacksore@users.noreply.github.com>
2023-12-06 12:53:42 -0600

    Fix path

--- index.js
+++ index.js
@@ -2,13 +2,6 @@
 
 import { execSync } from "child_process";
 
-// get the directory of the current file
 const __dirname = new URL(".", import.meta.url).pathname;
-
-const cwd = process.cwd();
-console.log({
-  cwd,
-  __dirname,
-});
 
 execSync(`npx tsx ${__dirname}/index.ts`, { stdio: "inherit" });
